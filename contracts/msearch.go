c2495a904aa89c7208f88dd0271ba5e80c338ac1
Tony An <anxuanzi@gmail.com>
2024-07-02 14:33:14 -0500

    :sparkles: Add Meilisearch interfaces in contracts
    This commit introduces a new file, msearch.go, in the contracts package. It defines an interface for Meilisearch, allowing various operations such as adding, deleting, and getting a document, applying index configurations, and tracking task success.

---
+++ contracts/msearch.go
@@ -0,0 +1,13 @@
+package contracts
+
+import "github.com/meilisearch/meilisearch-go"
+
+type Meilisearch interface {
+	ApplyIndexConfigs(configData []byte) error
+	WaitForTaskSuccess(taskUID int64) error
+	AddDoc(indexName string, docPtr any) error
+	DelDoc(indexName string, docId string) error
+	UpdateDoc(indexName string, docPtr any) error
+	GetDoc(indexName string, docId string, bindResult any) (bool, error)
+	Search(indexName string, query string, options *meilisearch.SearchRequest) *meilisearch.SearchResponse
+}
