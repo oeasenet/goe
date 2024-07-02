1bbab70f56474da2cff180bb18540ca63c416eef
Tony An <anxuanzi@gmail.com>
2024-07-02 14:35:38 -0500

    Move Logger interface to contracts package
    The Logger interface was previously existing in the modules log package. Now, it has been moved to the contracts package. The constructor in the log package has also been updated to reflect these changes. This new structure segregates the interface declaration from its implementation, improving code organization.

--- modules/log/handler.go
+++ modules/log/handler.go
@@ -5,99 +5,99 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-type log struct {
+type Log struct {
 	zapLogger *zap.Logger
 	zapSugar  *zap.SugaredLogger
 }
 
-func (jl *log) Debug(args ...any) {
+func (jl *Log) Debug(args ...any) {
 	jl.zapSugar.Debug(args...)
 }
 
-func (jl *log) Log(args ...any) {
+func (jl *Log) Log(args ...any) {
 	jl.zapSugar.Log(zapcore.InfoLevel, args...)
 }
 
-func (jl *log) Info(args ...any) {
+func (jl *Log) Info(args ...any) {
 	jl.zapSugar.Info(args...)
 }
 
-func (jl *log) Warn(args ...any) {
+func (jl *Log) Warn(args ...any) {
 	jl.zapSugar.Warn(args...)
 }
 
-func (jl *log) Error(args ...any) {
+func (jl *Log) Error(args ...any) {
 	jl.zapSugar.Error(args...)
 }
 
-func (jl *log) Fatal(args ...any) {
+func (jl *Log) Fatal(args ...any) {
 	jl.zapSugar.Fatal(args...)
 }
 
-func (jl *log) Panic(args ...any) {
+func (jl *Log) Panic(args ...any) {
 	jl.zapSugar.Panic(args...)
 }
 
-func (jl *log) Debugf(format string, args ...any) {
+func (jl *Log) Debugf(format string, args ...any) {
 	jl.zapSugar.Debugf(format, args...)
 }
 
-func (jl *log) Logf(format string, args ...any) {
+func (jl *Log) Logf(format string, args ...any) {
 	jl.zapSugar.Logf(zapcore.InfoLevel, format, args...)
 }
 
-func (jl *log) Infof(format string, args ...any) {
+func (jl *Log) Infof(format string, args ...any) {
 	jl.zapSugar.Infof(format, args...)
 }
 
-func (jl *log) Warnf(format string, args ...any) {
+func (jl *Log) Warnf(format string, args ...any) {
 	jl.zapSugar.Warnf(format, args...)
 }
 
-func (jl *log) Errorf(format string, args ...any) {
+func (jl *Log) Errorf(format string, args ...any) {
 	jl.zapSugar.Errorf(format, args...)
 }
 
-func (jl *log) Fatalf(format string, args ...any) {
+func (jl *Log) Fatalf(format string, args ...any) {
 	jl.zapSugar.Fatalf(format, args...)
 }
 
-func (jl *log) Panicf(format string, args ...any) {
+func (jl *Log) Panicf(format string, args ...any) {
 	jl.zapSugar.Panicf(format, args...)
 }
 
-func (jl *log) Debugw(msg string, keysAndValues ...any) {
+func (jl *Log) Debugw(msg string, keysAndValues ...any) {
 	jl.zapSugar.Debugw(msg, keysAndValues...)
 }
 
-func (jl *log) Infow(msg string, keysAndValues ...any) {
+func (jl *Log) Infow(msg string, keysAndValues ...any) {
 	jl.zapSugar.Infow(msg, keysAndValues...)
 }
 
-func (jl *log) Warnw(msg string, keysAndValues ...any) {
+func (jl *Log) Warnw(msg string, keysAndValues ...any) {
 	jl.zapSugar.Warnw(msg, keysAndValues...)
 }
 
-func (jl *log) Errorw(msg string, keysAndValues ...any) {
+func (jl *Log) Errorw(msg string, keysAndValues ...any) {
 	jl.zapSugar.Errorw(msg, keysAndValues...)
 }
 
-func (jl *log) Fatalw(msg string, keysAndValues ...any) {
+func (jl *Log) Fatalw(msg string, keysAndValues ...any) {
 	jl.zapSugar.Fatalw(msg, keysAndValues...)
 }
 
-func (jl *log) Panicw(msg string, keysAndValues ...any) {
+func (jl *Log) Panicw(msg string, keysAndValues ...any) {
 	jl.zapSugar.Panicw(msg, keysAndValues...)
 }
 
-func (jl *log) GetZapLogger() *zap.Logger {
+func (jl *Log) GetZapLogger() *zap.Logger {
 	return jl.zapLogger
 }
 
-func (jl *log) GetZapSugarLogger() *zap.SugaredLogger {
+func (jl *Log) GetZapSugarLogger() *zap.SugaredLogger {
 	return jl.zapSugar
 }
 
-func (jl *log) Close() {
+func (jl *Log) Close() {
 	jl.zapLogger.Sync()
 }
