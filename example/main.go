c492cd3fd51d456d4bb4d1e0cdd7873a5fd45cbd
Tony An <anxuanzi@gmail.com>
2024-07-07 03:02:15 -0500

    :sparkles: Add main function and environment configs
    A main function is added to the application, which initializes the app, sets up a simple "Hello, World!" route using the Fiber module, and then runs the app. Additionally, .env and .local.env configuration files are created to manage app settings including database (MongoDB, Redis), mailer (SMTP), object storage (S3), HTTP server (Fiber), and others.

--- example/main.go
+++ example/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"go.oease.dev/goe"
+)
+
+func main() {
+	// This is an example of a main function
+	// Create a new goe app, this will initialize the app and its dependencies
+	err := goe.NewApp()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Use the Fiber module to set up a simple hello world route
+	goe.UseFiber().App().Get("/hello", func(ctx fiber.Ctx) error {
+		return ctx.SendString("Hello, World!")
+	})
+
+	// Run the app, this will start the server and block the main thread. Graceful shutdown is supported.
+	err = goe.Run()
+	if err != nil {
+		panic(err)
+		return
+	}
+}
