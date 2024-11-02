aeabe40a05886eff54ed6fc55b0128292a4524ac
Tony An <anxuanzi@gmail.com>
2024-11-02 02:58:51 -0500

    :sparkles: Add configuration options for creating new queues
    Introduce the NewQueueCfg struct for customizable queue creation parameters. Default values are applied if the configuration is not provided, ensuring robustness and flexibility when setting up new queues.

--- core/queue.go
+++ core/queue.go
@@ -61,14 +61,54 @@ func (g *GoeQueue) Close() error {
 	return nil
 }
 
-func (g *GoeQueue) NewQueue(name contracts.QueueName, handler func(string) bool) {
+// NewQueueCfg is the configuration for creating a new queue
+type NewQueueCfg struct {
+	// ConcurrentWorkers is the number of concurrent workers to process the queue
+	ConcurrentWorkers int
+	// FetchInterval is the interval in seconds to fetch new jobs
+	FetchInterval int
+	// DefaultRetries is number of retries for a job, can be overridden when creating a message
+	DefaultRetries int
+	// MaxConsumeDuration is the maximum time in seconds to consume a job, if exceeded, the job will be retried
+	MaxConsumeDuration int
+	// FetchLimit is the maximum number of jobs to fetch in a single fetch, 0 means no limit
+	FetchLimit int
+}
+
+func (g *GoeQueue) NewQueue(name contracts.QueueName, handler func(string) bool, cfgs ...*NewQueueCfg) {
 	rq := queue.NewQueue(string(name), g.redisCli)
 	rq.WithCallback(handler)
-	rq.WithConcurrent(uint(g.goeConfig.Queue.ConcurrentWorkers))
-	rq.WithFetchInterval(time.Duration(g.goeConfig.Queue.FetchInterval) * time.Second)
-	rq.WithDefaultRetryCount(uint(g.goeConfig.Queue.DefaultRetries))
-	rq.WithMaxConsumeDuration(time.Duration(g.goeConfig.Queue.MaxConsumeDuration) * time.Second)
-	rq.WithFetchLimit(uint(g.goeConfig.Queue.FetchLimit))
+	if len(cfgs) == 0 && cfgs[0] != nil {
+		// if config is provided, use the provided config, if values are 0, use the default values
+		if cfgs[0].ConcurrentWorkers == 0 {
+			// default to 1 worker
+			cfgs[0].ConcurrentWorkers = 1
+		}
+		if cfgs[0].FetchInterval == 0 {
+			// default to 5 seconds
+			cfgs[0].FetchInterval = 5
+		}
+		if cfgs[0].DefaultRetries == 0 {
+			// default to 3 retries
+			cfgs[0].DefaultRetries = 3
+		}
+		if cfgs[0].MaxConsumeDuration == 0 {
+			// default to 60 seconds
+			cfgs[0].MaxConsumeDuration = 60
+		}
+		rq.WithConcurrent(uint(cfgs[0].ConcurrentWorkers))
+		rq.WithFetchInterval(time.Duration(cfgs[0].FetchInterval) * time.Second)
+		rq.WithDefaultRetryCount(uint(cfgs[0].DefaultRetries))
+		rq.WithMaxConsumeDuration(time.Duration(cfgs[0].MaxConsumeDuration) * time.Second)
+		rq.WithFetchLimit(uint(cfgs[0].FetchLimit))
+	} else {
+		// if no config is provided, use the default config from the app config
+		rq.WithConcurrent(uint(g.goeConfig.Queue.ConcurrentWorkers))
+		rq.WithFetchInterval(time.Duration(g.goeConfig.Queue.FetchInterval) * time.Second)
+		rq.WithDefaultRetryCount(uint(g.goeConfig.Queue.DefaultRetries))
+		rq.WithMaxConsumeDuration(time.Duration(g.goeConfig.Queue.MaxConsumeDuration) * time.Second)
+		rq.WithFetchLimit(uint(g.goeConfig.Queue.FetchLimit))
+	}
 	g.queues.Store(name, rq)
 }
 
